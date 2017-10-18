a04eae2864d40fe1f84ec69c0fb89d8ce1276fcc
oshan2csd <oshan2csd@gmail.com>
2017-10-17 22:02:10 -0700

    Bug 10 - spade not rolling issue

--- src/DiceValue.java
+++ src/DiceValue.java
@@ -22,7 +22,8 @@ public enum DiceValue {
 	}
 	
 	public static DiceValue getRandom() {
-		int random = RANDOM.nextInt(DiceValue.SPADE.ordinal());
+		//int random = RANDOM.nextInt(DiceValue.SPADE.ordinal());
+                int random = RANDOM.nextInt(DiceValue.VALUE_REPR_MAP.size());
 		return values()[random];
 	}
 	
